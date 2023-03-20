9974b4b8073891970d29292e80078394d9726bdf
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-03-20 22:09:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+const main = document.createElement("main");
+//document.body.append(main);
+main.remove();
+
+// const newHeader = document.getElementById("victory");
+// newHeader.h1 ='H1';
+
+// const newHeader2 = document.getElementById("victory");
+// newHeader.victory ='victory';
+
+// const newHeaders = document.getElementById("victory");
+// newHeader.innerHTML ='H1';
+
+const newHeader = document.createElement("h1");
+//newHeader.h1 = "H1"
+newHeader.victory = 'victory'
+newHeader.textContent = "Caren is the champion"
